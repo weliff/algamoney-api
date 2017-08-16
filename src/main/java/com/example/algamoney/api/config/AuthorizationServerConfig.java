2dc861c791ed31728ed2e418206a63bde6c01364
Weliff Lima <weliff.14@gmail.com>
2017-08-16 01:09:31 -0300

    adicionado refresh token no cookie

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -27,7 +27,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
                 .accessTokenValiditySeconds(1800)
-                .refreshTokenValiditySeconds(36000);
+                .refreshTokenValiditySeconds(36000 * 24);
     }
 
     @Override
