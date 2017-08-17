68c2282c4553187ff98b851bdfd3ae5b5e5d414a
Weliff Lima <weliff.14@gmail.com>
2017-08-17 00:10:04 -0300

    adicionado filtro para CORS

---
+++ src/main/java/com/algamoney/api/cors/CorsFilter.java
@@ -0,0 +1,41 @@
+package com.algamoney.api.cors;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+
+    private final static String origemPermitida = "http://localhost:8081";
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+
+        response.setHeader("Access-Control-Allow-Origin", origemPermitida);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if (request.getMethod().equals("OPTIONS") && origemPermitida.equals(request.getHeader("Origin"))) {
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            response.setHeader("Access-Control-Max-Age", "3600");
+
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
